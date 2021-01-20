2d7d4948fe1c2100b64eae21d2ec884ebf41a7cf
Twan van Paridon <70744506+tvp23@users.noreply.github.com>
2021-01-20 11:55:37 +0100

    sql script

---
+++ CRAdreslijst.sql
@@ -0,0 +1,12 @@
+create database cradreslijst;
+
+use cradreslijst;
+
+create table adres(
+    voornaam varchar(50),
+     achternaam varchar(50),
+      adres varchar(50),
+       huisnummer varchar(10),
+        woonplaats varchar(50),
+        postcode varchar(6)
+);
